9b8e2b6e0477511c26f7848093b8afa6fa1bc159
Shekinah Adeogo <adeogoshekinah@gmail.com>
2025-06-29 03:35:04 +0100

    feat: add routing for home, cart, and plant details

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import PlantDetails from './pages/PlantDetails';
+
 const App = () => {
-  return <div>PotStop</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/plant/:id' element={<PlantDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className='font-logo font-bold text-5xl'>PotStop</div>
+    </nav>
+  );
+};
+
+export default Navbar;
