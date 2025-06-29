5bd643e24bd257a557d3645c2fe4b6f5afbd04bd
Shekinah Adeogo <adeogoshekinah@gmail.com>
2025-06-29 12:31:19 +0100

    feat: implement plant details page with image slider and safelisted colors

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  safelist: [
+    'bg-yellow-400',
+    'bg-red-500',
+    'bg-purple-900',
+    'bg-green-800',
+    'bg-emerald-600',
+    'bg-yellow-300',
+    'bg-red-500',
+    'bg-pink-400',
+    'bg-violet-500',
+    'bg-gray-900',
+    'bg-pink-700',
+    'bg-blue-300',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
