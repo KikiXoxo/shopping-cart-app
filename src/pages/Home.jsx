030d5f921e0851bb396e405f48ef3d736fd36983
Shekinah Adeogo <adeogoshekinah@gmail.com>
2025-06-29 09:31:50 +0100

    feat: add hero section to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,8 +18,37 @@ const Home = () => {
   }, []);
 
   return (
-    <div className='py-12 '>
-      <div className='mx-4 md:mx-12 lg:max-w-[80%] lg:mx-auto '>
+    <div>
+      {/* Hero Section */}
+      <div
+        className='h-[70vh] lg:h-[calc(100vh-64px)] bg-cover bg-center flex items-center'
+        style={{
+          backgroundImage:
+            "linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url('/img/24.jpg')",
+        }}
+      >
+        <div className='w-[90%] lg:w-[80%] mx-auto text-center text-white flex flex-col items-center lg:items-start lg:text-left'>
+          <h1 className='font-heading font-bold text-5xl md:text-7xl italic mb-8'>
+            Welcome to PotStop
+          </h1>
+          <p className='text-lg md:text-xl mb-6 md:max-w-lg lg:max-w-2xl'>
+            Let's help you discover vibrant potted plants to brighten your space
+            and lift your mood.
+          </p>
+          <a
+            href='#shop-section'
+            className='inline-block bg-teal-600 text-white text-lg uppercase py-2 px-12 rounded hover:bg-teal-700 transition-transform duration-300 hover:scale-105 font-semibold'
+          >
+            Shop Now
+          </a>
+        </div>
+      </div>
+
+      {/* Plants List */}
+      <div
+        id='shop-section'
+        className='mx-4 md:mx-12 lg:max-w-[80%] lg:mx-auto py-12'
+      >
         <h2 className='font-heading text-3xl text-teal-900 font-bold text-center mb-4'>
           Shop Our Plants
         </h2>
