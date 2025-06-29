9a62c84989f9383e9a6bd12a5ba2570ee26b4ce3
Shekinah Adeogo <adeogoshekinah@gmail.com>
2025-06-29 17:59:13 +0100

    feat: add counter badge to navbar that displays total item count next to cart icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,12 @@ import {
   MdOutlineShoppingCart,
   MdOutlineMarkEmailUnread,
 } from 'react-icons/md';
+import { useCart } from '../context/CartContext';
 
 const Navbar = () => {
+  const { cart } = useCart();
+  const totalItems = cart.length;
+
   return (
     <nav className='w-full bg-teal-900'>
       <div className='max-w-[80%] mx-auto py-4 flex text-white justify-between items-center'>
@@ -13,8 +17,13 @@ const Navbar = () => {
         </Link>
 
         <div className='flex gap-4'>
-          <Link to='/cart'>
+          <Link to='/cart' className='relative'>
             <MdOutlineShoppingCart className='text-3xl hover:text-teal-100' />
+            {totalItems > 0 && (
+              <span className='absolute -top-2 -right-2 bg-gray-200 text-teal-800 text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center'>
+                {totalItems}
+              </span>
+            )}
           </Link>
           <a
             href='https://github.com/KikiXoxo'
